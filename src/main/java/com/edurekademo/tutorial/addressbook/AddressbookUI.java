1cfe55230c1185873e02b580292ac523fa2bc3c0
Noorulla94 <133254714+Noorulla94@users.noreply.github.com>
2025-07-22 13:38:29 +0530

    Update AddressbookUI.java

--- src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -79,7 +79,7 @@ public class AddressbookUI extends UI {
         contactList.setColumnOrder("firstName", "lastName", "email");
         contactList.removeColumn("id");
         contactList.removeColumn("birthDate");
-        contactList.removeColumn("phone");
+        contactList.removeColumn("phone github_webhook");
         contactList.setSelectionMode(Grid.SelectionMode.SINGLE);
         contactList.addSelectionListener(
                 e -> contactForm.edit((Contact) contactList.getSelectedRow()));
